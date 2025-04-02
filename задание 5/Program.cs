c23a38bf4e8c3602978493148f5a1a916d9428fb
kshnchk <kruglovaksusa31@gmail.com>
2025-04-02 19:22:57 +0400

    Add files via upload

---
+++ задание 5/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Создать на основе п.2 программу, которая вводит с
+клавиатуры числа a и b, указывающие диапазон случайных чисел и
+выводит случайное число из этого диапазонаю.
+*/
+
+namespace задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int numa = int.Parse(a);
+            int numb = int.Parse(b);
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(numa, numb));
+        }
+    }
+}
---
+++ задание 6/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Создайте 3 числа.
+float a = 3.2f;
+decimal b = 2.6m;
+double c = 4.5;
+Запишите в с сумму всех трёх чисел. Исправьте возникающие
+ошибки с помощью явного преобразования данных.
+*/
+
+namespace задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.2f;
+            decimal b = 2.6m;
+            double c = 4.5;
+            c += (double)a + (double)b;
+            Console.WriteLine(c);
+        }
+    }
+}
