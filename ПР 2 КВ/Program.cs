e1b22c28f193e03a9937b92064d888f737032b3c
kshnchk <kruglovaksusa31@gmail.com>
2025-04-05 18:36:05 +0400

    Add files via upload

--- ПР 2 КВ/Program.cs
+++ ПР 2 КВ/Program.cs
@@ -30,7 +30,7 @@ namespace задание_1_4
     internal class Program
     {
         static void Main(string[] args)
-        {
+  {
         Random rnd = new Random();
         Console.WriteLine(rnd.Next()); 
         Console.WriteLine(rnd.Next(5)); 
